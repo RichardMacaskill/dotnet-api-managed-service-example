459d02ecf22f90a93f5df1eb242b98fc3799a5fd
Richard Macaskill <richard.macaskill@neo-technology.com>
2023-08-08 13:08:13 +0100

    add null checking and non-code files

--- Program.cs
+++ Program.cs
@@ -2,28 +2,39 @@
 using System.Text.Json;
 
 var uri = Environment.GetEnvironmentVariable("NEO4J_URI");
-var loginUri = Environment.GetEnvironmentVariable("NEO4J_AAD_LoginUri"); // this is the MS API to obtain a token, e.g. https://login.microsoftonline.com/<your-tenancy-id>/oauth2/token 
-var clientId = Environment.GetEnvironmentVariable("NEO4J_ServicePrincipal_ClientId"); // the client secret id for the MSP
-var clientSecret = Environment.GetEnvironmentVariable("NEO4J_ServicePrincipal_ClientSecret"); // the client secret value for the MSP
-var resource = Environment.GetEnvironmentVariable("NEO4J_AAD_Application_Scope"); // the scope for the token request e.g. <your enterprise application id>/Token.Connect
+var loginUri =
+    Environment.GetEnvironmentVariable(
+        "NEO4J_AAD_LoginUri"); // this is the MS API to obtain a token, e.g. https://login.microsoftonline.com/<your-tenancy-id>/oauth2/token 
+var clientId =
+    Environment.GetEnvironmentVariable("NEO4J_ServicePrincipal_ClientId"); // the client secret id for the MSP
+var clientSecret =
+    Environment.GetEnvironmentVariable("NEO4J_ServicePrincipal_ClientSecret"); // the client secret value for the MSP
+var resource =
+    Environment.GetEnvironmentVariable(
+        "NEO4J_AAD_Application_Scope"); // the scope for the token request e.g. <your enterprise application id>/Token.Connect
 
 /* Using a stored password (Client Secret) for the ServicePrincipal, obtain a jwt token to pass to Neo4j 
 NOTE: MS recommend to use a certificate for Production deployments */
 
 var client = new HttpClient();
 var request = new HttpRequestMessage(HttpMethod.Post, loginUri);
-var collection = new List<KeyValuePair<string, string>>
+
+if (clientSecret != null && clientId != null && resource != null)
 {
-    /* Build the request to the AAD Login uri */
+    var collection = new List<KeyValuePair<string, string>>
+    {
+        /* Build the request to the AAD Login uri */
 
-    new("grant_type", "client_credentials"),
-    new("client_id", clientId),
-    new("client_secret", clientSecret),
-    new("resource", resource)
-};
-var content = new FormUrlEncodedContent(collection);
+        new("grant_type", "client_credentials"),
+        new("client_id", clientId),
+        new("client_secret", clientSecret),
+        new("resource", resource)
+    };
+    var content = new FormUrlEncodedContent(collection);
 
-request.Content = content;
+    request.Content = content;
+}
+
 
 /* Make the login API call to obtain a jwt access token, and extract it from the result json */
 
